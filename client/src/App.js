f0dbec59faf660b7f92de77b066fe4900834a3d4
ron <rinbar70@gmail.com>
2020-09-11 20:41:57 +0300

    Create <HomePage>, <LoginForm>, <SignupForm>.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import HomePage from './components/home-page';
+import LoginForm from './components/login-form';
+import SignupForm from './components/signup-form';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/signup">
-          <h1>Sign Up</h1>
+          <SignupForm />
+          <Link to="/login">Already have an account?</Link>
         </Route>
         <Route exact path="/login">
-          <h1>Log In</h1>
+          <LoginForm />
+          <Link to="/signup">Don't have an account yet?</Link>
         </Route>
         <Route exact path="/">
-          <h1>Homepage</h1>
+          <HomePage />
         </Route>
       </Switch>
     </BrowserRouter>
---
+++ client/src/components/signup-form.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function SignupForm() {
+    return (<h1>Sign Up</h1>);
+}
