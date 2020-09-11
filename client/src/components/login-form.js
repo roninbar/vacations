dd11e705b6ed730f1da040ebe0d616c5d416513d
ron <rinbar70@gmail.com>
2020-09-11 20:46:59 +0300

    Copy <LoginForm> and <SignupForm> from the To Do List app.

--- client/src/components/login-form.js
+++ client/src/components/login-form.js
@@ -1,5 +1,80 @@
-import React from 'react';
+import { Button } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
-export default function LoginForm() {
-    return (<h1>Log In</h1>);
+class LoginForm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+        };
+    }
+
+    async onSubmit(e) {
+        e.preventDefault();
+        const { username, password } = this.state;
+        const body = new URLSearchParams();
+        body.set('username', username);
+        body.set('password', password);
+        const { status } = await fetch('/login', {
+            method: 'POST',
+            body,
+        });
+        const { history } = this.props;
+        if (history && 200 <= status && status < 300) {
+            history.push('/');
+        }
+    }
+
+    onChangeField({ target: { name, value } }) {
+        this.setState({ [name]: value });
+    }
+
+    render() {
+        const { classes } = this.props;
+        const { username, password } = this.state;
+        return (
+            <form className={classes.root} noValidate autoComplete="off" onSubmit={this.onSubmit.bind(this)}>
+                <TextField
+                    variant="outlined"
+                    type="text"
+                    name="username"
+                    label="Username"
+                    value={username}
+                    onChange={this.onChangeField.bind(this)} />
+                <TextField
+                    variant="outlined"
+                    type="password"
+                    name="password"
+                    label="Password"
+                    value={password}
+                    onChange={this.onChangeField.bind(this)}
+                />
+                <Button type="submit"
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    disabled={!username || !password}
+                >Log In</Button>
+            </form>
+        );
+    }
+
 }
+
+const styles = theme => ({
+    root: {
+        '& .MuiTextField-root, & .MuiButton-root': {
+            margin: theme.spacing(1),
+        },
+        '& .MuiTextField-root': {
+            width: '25ch',
+        },
+    },
+});
+
+export default withRouter(withStyles(styles)(LoginForm));
--- client/src/components/signup-form.js
+++ client/src/components/signup-form.js
@@ -1,5 +1,96 @@
-import React from 'react';
+import { Button, TextField, withStyles } from '@material-ui/core';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
-export default function SignupForm() {
-    return (<h1>Sign Up</h1>);
+class SignupForm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            password2: '',
+            available: true,
+        };
+    }
+
+    onChange({ target: { name, value } }) {
+        this.setState({ [name]: value });
+    }
+
+    async onSubmit(e) {
+        e.preventDefault();
+        const { username, password } = this.state;
+        const body = new URLSearchParams();
+        body.set('username', username);
+        body.set('password', password);
+        await fetch('/user', {
+            method: 'POST',
+            body,
+        });
+    }
+
+    render() {
+        const { username, password, password2, available } = this.state;
+        const { classes } = this.props;
+        return (
+            <form className={classes.root} noValidate autoComplete="off" onSubmit={this.onSubmit.bind(this)}>
+                <TextField
+                    variant="outlined"
+                    type="text"
+                    name="username"
+                    label="Username"
+                    error={!username || !available}
+                    value={username}
+                    onChange={this.onChange.bind(this)}
+                />
+                <TextField
+                    variant="outlined"
+                    type="password"
+                    name="password"
+                    label="Password"
+                    error={!password}
+                    value={password}
+                    onChange={this.onChange.bind(this)}
+                />
+                <TextField
+                    variant="outlined"
+                    type="password"
+                    name="password2"
+                    label="Repeat Password"
+                    error={password !== password2}
+                    value={password2}
+                    onChange={this.onChange.bind(this)}
+                />
+                <Button
+                    variant="contained"
+                    type="submit"
+                    color="primary"
+                    size="large"
+                >Sign Up</Button>
+            </form>
+        );
+    }
+
+    async componentDidUpdate(prevProps, { username: prevUsername }) {
+        const { username } = this.state;
+        if (username && username !== prevUsername) {
+            const { status } = await fetch(`/user/${username}`, { method: 'HEAD' });
+            this.setState({ available: status === 404 });
+        }
+    }
+
 }
+
+const styles = theme => ({
+    root: {
+        '& .MuiTextField-root, & .MuiButton-root': {
+            margin: theme.spacing(1),
+        },
+        '& .MuiTextField-root': {
+            width: '25ch',
+        },
+    },
+});
+
+export default withRouter(withStyles(styles)(SignupForm));
