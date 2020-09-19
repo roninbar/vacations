b02b8b4f567d61553264ec7fb3bc2acef4ba8579
ron <rinbar70@gmail.com>
2020-09-19 15:43:46 +0300

    Add user to state.

--- client-redux/src/app/store.js
+++ client-redux/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import { reduceUser } from '../reducers/user';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    user: reduceUser,
   },
 });
