50f06c2a4d7afbd5047f9868001a9a64b54730c8
ron <rinbar70@gmail.com>
2020-09-19 21:25:50 +0300

    Refactor the verify callback.

--- server/util/data/user/retrieve.js
+++ server/util/data/user/retrieve.js
@@ -1,17 +1,23 @@
-const mysql = require('mysql2/promise');
+var mysql = require('mysql2/promise');
+const { hash } = require('../../hash');
 
-async function getUserByName(name) {
-  const conn = await mysql.createConnection({
-    user: 'root',
-    database: 'vacations',
-  });
-  try {
-    const [[user]] = await conn.execute('SELECT * FROM `user` WHERE `name` = ?', [name]);
-    return user;
-  }
-  finally {
-    await conn.end();
-  }
+async function getUserByCredentials(username, password) {
+    const conn = await mysql.createConnection({
+        user: 'root',
+        database: 'vacations',
+    });
+    try {
+        const [[user]] = await conn.execute(
+            'SELECT `user`.`id` AS `id`, `user`.`name` as `name`, `role`.`name` AS `role` ' +
+            'FROM `user` JOIN `role` ON `role_id` = `role`.`id` ' +
+            'WHERE `user`.`name` = ? AND `password_hash` = ?',
+            [username, hash(password)]);
+        return user;
+    }
+    finally {
+        await conn.end();
+    }
 }
 
-exports.getUserByName = getUserByName;
+exports.getUserByCredentials = getUserByCredentials;
+
--- server/util/passport.js
+++ server/util/passport.js
@@ -1,26 +1,13 @@
 var passport = require('passport');
 var { Strategy } = require('passport-local');
-var mysql = require('mysql2/promise');
-const { hash } = require('./hash');
+const { getUserByCredentials } = require("./data/user/retrieve");
 
 passport.use(new Strategy(async function (username, password, done) {
-    const conn = await mysql.createConnection({
-        user: 'root',
-        database: 'vacations',
-    });
     try {
-        const [[user]] = await conn.execute(
-            'SELECT `user`.`id` AS `id`, `user`.`name` as `name`, `role`.`name` AS `role` ' +
-            'FROM `user` JOIN `role` ON `role_id` = `role`.`id` ' +
-            'WHERE `user`.`name` = ? AND `password_hash` = ?',
-            [username, hash(password)]);
-        return done(null, user, { message: user ? 'Greetings, Professor Falken!' : 'Invalid username/password.' });
+        return done(null, await getUserByCredentials(username, password));
     }
     catch (error) {
         return done(error);
-    }
-    finally {
-        await conn.end();
     }
 }));
 
@@ -33,3 +20,4 @@ passport.deserializeUser(function (id, done) {
 });
 
 module.exports = passport;
+
