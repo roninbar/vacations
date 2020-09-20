ad5e8f040e93a99a60c28a88ae4709012e79050f
ron <rinbar70@gmail.com>
2020-09-21 00:04:12 +0300

    Implement "PUT /vacation/:id/follow" and "DELETE /vacation/:id/follow".

---
+++ entities/vacation/update.js
@@ -0,0 +1,32 @@
+const mysql = require('mysql2/promise');
+
+async function followVacation(userId, vacationId) {
+    const conn = await mysql.createConnection({
+        user: 'root',
+        database: 'vacations',
+    });
+    try {
+        const [{ insertId }] = await conn.execute('INSERT IGNORE INTO `user_vacation` (`user_id`, `vacation_id`) VALUES (?, ?)', [userId, vacationId]);
+        return insertId;
+    }
+    finally {
+        await conn.end();
+    }
+}
+
+async function unfollowVacation(userId, vacationId) {
+    const conn = await mysql.createConnection({
+        user: 'root',
+        database: 'vacations',
+    });
+    try {
+        const [{ affectedRows }] = await conn.execute('DELETE FROM `user_vacation` WHERE `user_id` = ? AND `vacation_id` = ?', [userId, vacationId]);
+        return affectedRows;
+    }
+    finally {
+        await conn.end();
+    }
+}
+
+Object.assign(exports, { followVacation, unfollowVacation });
+
--- routes/vacations.js
+++ routes/vacations.js
@@ -1,10 +1,23 @@
 var { getAllVacations } = require('../entities/vacation/retrieve');
+var { followVacation, unfollowVacation } = require('../entities/vacation/update');
 var express = require('express');
 var router = express.Router();
 
 // GET all the vacations.
-router.get('/all', async function (req, res, next) {
-    res.json(await getAllVacations(req.user));
+router.get('/all', async function ({ user }, res) {
+    res.json(await getAllVacations(user));
+});
+
+// Follow vacation.
+router.put('/:id/follow', async function ({ params: { id: vacationId }, user: { id: userId } }, res) {
+    const insertId = await followVacation(userId, vacationId);
+    res.sendStatus(insertId > 0 ? 201 : 204);
+});
+
+// Unfollow vacation.
+router.delete('/:id/follow', async function ({ params: { id: vacationId }, user: { id: userId } }, res) {
+    const affectedRows = await unfollowVacation(userId, vacationId);
+    res.sendStatus(affectedRows > 0 ? 204 : 404);
 });
 
 module.exports = router;
