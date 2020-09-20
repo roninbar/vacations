d6716a16b0a79bbe62a1244790a36c0d55df092e
ron <rinbar70@gmail.com>
2020-09-20 15:31:08 +0300

    Truncate the vacations array when the user logs out.

--- client-redux/src/reducers/vacations.js
+++ client-redux/src/reducers/vacations.js
@@ -1,3 +1,4 @@
+import { LOGOUT } from '../actions/user';
 import { RECEIVE_VACATIONS, REQUEST_VACATIONS, UNKNOWN_ERROR } from '../actions/vacations';
 
 export function reduceVacations({ error, loading, vacations } = { error: false, loading: false, vacations: [] }, { type, payload }) {
@@ -8,6 +9,8 @@ export function reduceVacations({ error, loading, vacations } = { error: false, 
             return { error: false, loading: false, vacations: payload };
         case UNKNOWN_ERROR:
             return { error: true, loading: false, vacations };
+        case LOGOUT:
+            return { error: false, loading: false, vacations: [] };
         default:
             return { error, loading, vacations };
     }
