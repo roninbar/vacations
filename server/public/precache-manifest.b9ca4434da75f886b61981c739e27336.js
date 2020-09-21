7514018e27aa86430abe7a6a2a44dc4aa758234b
ron <rinbar70@gmail.com>
2020-09-21 17:47:26 +0300

    Update /server/public.

---
+++ server/public/precache-manifest.b9ca4434da75f886b61981c739e27336.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe4b29d0bfc0c590cbaaa0772d83132f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27486103c872d2426a0b",
+    "url": "/static/css/main.0e96c88f.chunk.css"
+  },
+  {
+    "revision": "d0a15c2711e71821da4a",
+    "url": "/static/js/2.7c632019.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7c632019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27486103c872d2426a0b",
+    "url": "/static/js/main.73045444.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
