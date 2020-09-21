079d522ff4500e3cb1f1a327c57aa255277bab31
ron <rinbar70@gmail.com>
2020-09-21 14:25:29 +0300

    Add boolean field "is_following" to each vacation in "GET /vacation/all". (Can't amend the previous commit because I already pushed it.)

--- server/entities/vacation/retrieve.js
+++ server/entities/vacation/retrieve.js
@@ -1,16 +1,19 @@
 const mysql = require('mysql2/promise');
 
-async function getAllVacations() {
+async function getAllVacations(userId) {
     const conn = await mysql.createConnection({
         user: 'root',
         database: 'vacations',
     });
     try {
         const [vacations] = await conn.execute(
-            'SELECT `vacation`.*, COUNT(`user_id`) AS `followers` ' +
+            'SELECT `vacation`.*, ' +
+            'COUNT(`user_id`) AS `followers`, ' +
+            'IFNULL(MAX(`user_id` = ?), 0) AS `isFollowing` ' +
             'FROM `user_vacation` ' +
             'RIGHT JOIN `vacation` ON `vacation_id` = `vacation`.`id` ' +
-            'GROUP BY `vacation`.`id`'
+            'GROUP BY `vacation`.`id`',
+            [userId],
         );
         return vacations;
     }
