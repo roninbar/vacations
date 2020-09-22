7205e0947e9974bebc32bffd726fc8e1b36e9bb5
ron <rinbar70@gmail.com>
2020-09-22 22:42:30 +0300

    Get rid of the Counter feature.

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from 'features/counter/counterSlice';
 import userReducer from 'features/userSlice';
 import vacationsReducer from 'features/vacationsSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     user: userReducer,
     vacations: vacationsReducer,
   },
