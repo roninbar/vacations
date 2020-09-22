5cafdb784d2b247577816cd36563676f3e43fc08
ron <rinbar70@gmail.com>
2020-09-22 22:47:10 +0300

    Build client and update /server/public.

--- server/public/precache-manifest.34438a7dd8fd2678926835fbe45e8582.js
+++ server/public/precache-manifest.34438a7dd8fd2678926835fbe45e8582.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fe4b29d0bfc0c590cbaaa0772d83132f",
+    "revision": "0505dbe749556d5c74a36eb08117f2c3",
     "url": "/index.html"
   },
   {
-    "revision": "27486103c872d2426a0b",
+    "revision": "888c2e34adc4df1a618e",
     "url": "/static/css/main.0e96c88f.chunk.css"
   },
   {
-    "revision": "d0a15c2711e71821da4a",
-    "url": "/static/js/2.7c632019.chunk.js"
+    "revision": "4e76ba63796976ace637",
+    "url": "/static/js/2.80244149.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/static/js/2.7c632019.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.80244149.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "27486103c872d2426a0b",
-    "url": "/static/js/main.73045444.chunk.js"
+    "revision": "888c2e34adc4df1a618e",
+    "url": "/static/js/main.bc050890.chunk.js"
   },
   {
     "revision": "9b4e396c183e42c1fa5c",
