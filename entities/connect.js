0ca69cf79384cb277e89742c1644ef19be3dff18
ron <rinbar70@gmail.com>
2020-09-26 17:57:49 +0300

    Use a connection pool; read DB user credentials from $USER and $PASS.

--- entities/connect.js
+++ entities/connect.js
@@ -1,10 +1,19 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 
-async function getSqlConnection() {
-    return await mysql.createConnection({
-        user: 'root',
-        database: 'vacations',
-    });
-}
+const getSqlConnection = (function () {
+    let pool = null;
+    return function () {
+        if (!pool) {
+            pool = mysql.createPool({
+                user: process.env['USER'],
+                password: process.env['PASS'],
+                database: 'vacations',
+            });
+        }
+        const conn = Object.create(pool.promise());
+        conn.end = Function.prototype; // no-op
+        return conn;
+    };
+})();
 
 exports.getSqlConnection = getSqlConnection;
