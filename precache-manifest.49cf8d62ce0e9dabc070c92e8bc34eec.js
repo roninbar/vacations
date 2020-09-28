aff140b22caa56d96cce5dd44f602f5fbbf7a198
ron <rinbar70@gmail.com>
2020-09-28 13:46:43 +0300

    Updates

---
+++ precache-manifest.49cf8d62ce0e9dabc070c92e8bc34eec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7de2e15c2086ef6da55800a6923beeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be6c91dde1e95a4b399a",
+    "url": "/static/css/main.0e96c88f.chunk.css"
+  },
+  {
+    "revision": "4cc796e4092feb42aaae",
+    "url": "/static/js/2.61e16a2b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.61e16a2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be6c91dde1e95a4b399a",
+    "url": "/static/js/main.d9673bb0.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
