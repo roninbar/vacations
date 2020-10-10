c4ca5ca3c9b71d68f4bc8742dc42d296879bc48c
ron <rinbar70@gmail.com>
2020-10-10 17:25:44 +0300

    Change <AppBar position="fixed">.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { AppBar, Button, Container, IconButton, Toolbar, Typography, withStyles } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
-import { logOutAsync } from 'features/userSlice';
+import { logInAsync, logOutAsync } from 'features/userSlice';
 import React from 'react';
 import { connect } from 'react-redux';
 import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
@@ -14,7 +14,7 @@ function App({ classes, username, logOutAsync }) {
   return (
     <BrowserRouter>
       <Container maxWidth="xl" className={classes.root}>
-        <AppBar position="static">
+        <AppBar position="fixed">
           <Toolbar>
             <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
               <MenuIcon />
@@ -25,42 +25,45 @@ function App({ classes, username, logOutAsync }) {
               <Button color="inherit">Log In</Button>}
           </Toolbar>
         </AppBar>
-        <Switch>
-          <Route exact path="/signup">
-            <SignupForm />
-            <Link to="/login">Already have an account?</Link>
-          </Route>
-          <Route exact path="/login">
-            <LoginForm />
-            <Link to="/signup">Don't have an account yet?</Link>
-          </Route>
-          <PrivateRoute exact path="/" component={HomePage} />
-        </Switch>
+        {/* Render another <Toolbar> to prevent the <AppBar> from hiding some content. */}
+        {/* See https://material-ui.com/components/app-bar/#fixed-placement. */}
+        <Toolbar />
+          <Switch>
+            <Route exact path="/signup">
+              <SignupForm />
+              <Link to="/login">Already have an account?</Link>
+            </Route>
+            <Route exact path="/login">
+              <LoginForm />
+              <Link to="/signup">Don't have an account yet?</Link>
+            </Route>
+            <PrivateRoute exact path="/" component={HomePage} />
+          </Switch>
       </Container>
     </BrowserRouter>
   );
 }
 
 const styles = theme => ({
-  root: {
-    flexGrow: 1,
+          root: {
+          flexGrow: 1,
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+          marginRight: theme.spacing(2),
   },
   title: {
-    flexGrow: 1,
+          flexGrow: 1,
   },
   logout: {
-    float: 'right',
+          float: 'right',
   },
 });
 
-function mapStateToProps({ user: { name: username } }) {
-  return { username };
+function mapStateToProps({user: {name: username } }) {
+  return {username};
 }
 
-const mapDispatchToProps = { logOutAsync };
+const mapDispatchToProps = {logInAsync, logOutAsync};
 
 const withRedux = connect(mapStateToProps, mapDispatchToProps);
 
