7c59ce59e9a3d1b06bf10dc7f2c606f70e7534df
ron <rinbar70@gmail.com>
2020-10-11 22:18:07 +0300

    Add user.

--- sql/vacations.sql
+++ sql/vacations.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 10, 2020 at 11:52 PM
+-- Generation Time: Oct 11, 2020 at 09:14 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.9
 
@@ -94,7 +94,7 @@ INSERT INTO `user` (`id`, `name`, `password_hash`, `first_name`, `last_name`, `r
 (28, 'uri', '+OZPF0NlweGlAV+4kh5pZh1ZKB7HbsKvVaM54XoNS7Q=', 'Uri', 'Inbar', 1),
 (29, 'admin', 'G9a/pOtmKqmPqNm1Q8nBG1y/K7h1prIjizOM0E7vMvM=', 'Ron', 'Inbar', 2),
 (30, 'namir', '+OZPF0NlweGlAV+4kh5pZh1ZKB7HbsKvVaM54XoNS7Q=', 'Namir', 'Inbar', 1),
-(31, 'doovshanit', '+OZPF0NlweGlAV+4kh5pZh1ZKB7HbsKvVaM54XoNS7Q=', 'Doovshanit', 'Inbar', 1);
+(41, 'user', 'wwVD5HfVXD3DTGXU1UMHW7exMySigpo2KxD89BaMOBI=', 'Ron', 'Inbar', 1);
 
 -- --------------------------------------------------------
 
@@ -122,16 +122,15 @@ CREATE TABLE `user_vacation` (
 
 INSERT INTO `user_vacation` (`id`, `user_id`, `vacation_id`) VALUES
 (403, 26, 1),
-(404, 26, 2),
-(405, 26, 3),
-(406, 26, 17),
+(421, 26, 21),
 (410, 27, 1),
 (411, 27, 2),
 (409, 27, 3),
 (408, 28, 1),
 (407, 28, 3),
+(420, 30, 2),
 (412, 30, 3),
-(413, 30, 17);
+(417, 33, 1);
 
 -- --------------------------------------------------------
 
@@ -161,7 +160,8 @@ INSERT INTO `vacation` (`id`, `destination`, `from`, `to`, `price`, `description
 (1, 'Coyhaique', '2020-11-01', '2020-11-05', '5000.00', 'If adventure runs through your veins, this is the right place for you. This corner of Patagonia challenges you to climb its amazing peaks, like the Mackay or the Castillo. Trekking is a must and fly fishing gives you time to relax on the amazing rivers. Wonder at the untouched nature of this area in the heart of Patagonia, with its evergreen forests and lengthy Carretera Austral, which will captivate you with beautiful views. This is the perfect destination if you want to push your senses to the limit and get in touch with your wild side. Visit the arts and crafts fair in Coyhaique’s Main Square. You’ll find handmade products on the stands made with materials like native wood, leather, stone and wool, souvenirs that will remind you of your trip through the south of .', '/images/foto-centro-coyhaique--696x426.jpg'),
 (2, 'Puerto Aysén', '2021-01-01', '2020-11-15', '6500.00', 'Have you imagined how life goes on in the fjords of Patagonia? Here we will tell you the history of Puerto Aysén, a town located on the banks of the river, a few kilometers from the sea and surrounded by huge mountains full of evergreen forests. It is an almost pristine place, where 80% of the territory has not been explored by humans. This presents a tremendous challenge for its people, Chileans who are united to receive tourists and show them the best of Puerto Aysén.', '/images/4066224Master.jpg'),
 (3, 'Punta Arenas', '2021-01-01', '2021-12-31', '9999.98', 'With port and sheep-breeding history, the gateway into Antartica will captivate you with its clean air and sophisticated European architecture. Take a boat or a kayak along the Magellan Straits and you’ll be surprised by the penguins as they welcome you to Magdalena Island. Go on an adventure to Tierra del Fuego and discover the best kept secrets of the Kawesqar and Selk’nam people, indigenous to the southernmost part of Chile.', '/images/MG_9778.jpg'),
-(17, 'Vancouver', '2021-01-01', '2021-01-07', '1000.00', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore consequuntur assumenda aspernatur dolore voluptatum nesciunt doloremque velit officia eius quia! Laudantium facere similique fugiat pariatur est. Quod repudiandae eveniet nihil.', 'https://res.cloudinary.com/simpleview/image/upload/v1486505969/clients/vancouverbc/Aerial_Sunset_Vancouver_d3_copy_1bb86ed0-1edc-4cda-841d-0b033ca0bb72.jpg');
+(17, 'Vancouver', '2021-01-01', '2021-01-07', '1000.00', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore consequuntur assumenda aspernatur dolore voluptatum nesciunt doloremque velit officia eius quia! Laudantium facere similique fugiat pariatur est. Quod repudiandae eveniet nihil.', 'https://res.cloudinary.com/simpleview/image/upload/v1486505969/clients/vancouverbc/Aerial_Sunset_Vancouver_d3_copy_1bb86ed0-1edc-4cda-841d-0b033ca0bb72.jpg'),
+(21, 'Nepal', '2021-01-01', '2021-01-07', '2000.00', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore consequuntur assumenda aspernatur dolore voluptatum nesciunt doloremque velit officia eius quia! Laudantium facere similique fugiat pariatur est. Quod repudiandae eveniet nihil.', 'https://mk0cs00242yfx7ww7i54.kinstacdn.com/wp-content/uploads/sites/5/nepal-village.jpg');
 
 --
 -- Indexes for dumped tables
@@ -217,19 +217,19 @@ ALTER TABLE `role`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT for table `user_vacation`
 --
 ALTER TABLE `user_vacation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=414;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=422;
 
 --
 -- AUTO_INCREMENT for table `vacation`
 --
 ALTER TABLE `vacation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- Constraints for dumped tables
