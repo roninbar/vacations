10c54968ad7d6dd5d0a81b1663c6f120033b449b
ron <rinbar70@gmail.com>
2020-10-12 23:39:24 +0300

    Add a log message.

--- server/entities/connect.js
+++ server/entities/connect.js
@@ -1,10 +1,12 @@
 /* eslint-disable dot-notation */
 const mysql = require('mysql2/promise');
+const debug = require('debug')('server:mysql');
 
 const getSqlConnection = (function () {
     let pool = null;
     return async function () {
         if (!pool) {
+            debug(`Connecting to MySQL server at ${process.env['DBHOST'] || 'localhost'}:${process.env['DBPORT'] || '3306'}...`);
             pool = mysql.createPool({
                 host: process.env['DBHOST'],
                 port: process.env['DBPORT'],
