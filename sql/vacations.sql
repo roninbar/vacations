02b5fdbb34b8db0e71c5889612ab04090a98eafe
ron <rinbar70@gmail.com>
2020-10-16 15:54:33 +0300

    Add/modify destinations.

--- sql/vacations.sql
+++ sql/vacations.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 11, 2020 at 09:14 PM
+-- Generation Time: Oct 16, 2020 at 02:51 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.9
 
@@ -35,10 +35,6 @@ CREATE TABLE `role` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- RELATIONSHIPS FOR TABLE `role`:
---
-
---
 -- Dumping data for table `role`
 --
 
@@ -58,10 +54,6 @@ CREATE TABLE `sessions` (
   `data` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- RELATIONSHIPS FOR TABLE `sessions`:
---
-
 -- --------------------------------------------------------
 
 --
@@ -76,12 +68,6 @@ CREATE TABLE `user` (
   `last_name` varchar(64) NOT NULL,
   `role_id` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- RELATIONSHIPS FOR TABLE `user`:
---   `role_id`
---       `role` -> `id`
---
 
 --
 -- Dumping data for table `user`
@@ -109,20 +95,11 @@ CREATE TABLE `user_vacation` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- RELATIONSHIPS FOR TABLE `user_vacation`:
---   `user_id`
---       `user` -> `id`
---   `vacation_id`
---       `vacation` -> `id`
---
-
---
 -- Dumping data for table `user_vacation`
 --
 
 INSERT INTO `user_vacation` (`id`, `user_id`, `vacation_id`) VALUES
-(403, 26, 1),
-(421, 26, 21),
+(455, 26, 2),
 (410, 27, 1),
 (411, 27, 2),
 (409, 27, 3),
@@ -130,7 +107,14 @@ INSERT INTO `user_vacation` (`id`, `user_id`, `vacation_id`) VALUES
 (407, 28, 3),
 (420, 30, 2),
 (412, 30, 3),
-(417, 33, 1);
+(456, 31, 27),
+(417, 33, 1),
+(441, 41, 1),
+(438, 41, 2),
+(442, 41, 3),
+(443, 41, 17),
+(444, 41, 23),
+(445, 41, 27);
 
 -- --------------------------------------------------------
 
@@ -149,19 +133,17 @@ CREATE TABLE `vacation` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- RELATIONSHIPS FOR TABLE `vacation`:
---
-
---
 -- Dumping data for table `vacation`
 --
 
 INSERT INTO `vacation` (`id`, `destination`, `from`, `to`, `price`, `description`, `image`) VALUES
-(1, 'Coyhaique', '2020-11-01', '2020-11-05', '5000.00', 'If adventure runs through your veins, this is the right place for you. This corner of Patagonia challenges you to climb its amazing peaks, like the Mackay or the Castillo. Trekking is a must and fly fishing gives you time to relax on the amazing rivers. Wonder at the untouched nature of this area in the heart of Patagonia, with its evergreen forests and lengthy Carretera Austral, which will captivate you with beautiful views. This is the perfect destination if you want to push your senses to the limit and get in touch with your wild side. Visit the arts and crafts fair in Coyhaique’s Main Square. You’ll find handmade products on the stands made with materials like native wood, leather, stone and wool, souvenirs that will remind you of your trip through the south of .', '/images/foto-centro-coyhaique--696x426.jpg'),
+(1, 'Coyhaique', '2020-11-01', '2020-12-31', '5000.00', 'If adventure runs through your veins, this is the right place for you. This corner of Patagonia challenges you to climb its amazing peaks, like the Mackay or the Castillo. Trekking is a must and fly fishing gives you time to relax on the amazing rivers. Wonder at the untouched nature of this area in the heart of Patagonia, with its evergreen forests and lengthy Carretera Austral, which will captivate you with beautiful views. This is the perfect destination if you want to push your senses to the limit and get in touch with your wild side. Visit the arts and crafts fair in Coyhaique’s Main Square. You’ll find handmade products on the stands made with materials like native wood, leather, stone and wool, souvenirs that will remind you of your trip through the south of Chile.', '/images/foto-centro-coyhaique--696x426.jpg'),
 (2, 'Puerto Aysén', '2021-01-01', '2020-11-15', '6500.00', 'Have you imagined how life goes on in the fjords of Patagonia? Here we will tell you the history of Puerto Aysén, a town located on the banks of the river, a few kilometers from the sea and surrounded by huge mountains full of evergreen forests. It is an almost pristine place, where 80% of the territory has not been explored by humans. This presents a tremendous challenge for its people, Chileans who are united to receive tourists and show them the best of Puerto Aysén.', '/images/4066224Master.jpg'),
-(3, 'Punta Arenas', '2021-01-01', '2021-12-31', '9999.98', 'With port and sheep-breeding history, the gateway into Antartica will captivate you with its clean air and sophisticated European architecture. Take a boat or a kayak along the Magellan Straits and you’ll be surprised by the penguins as they welcome you to Magdalena Island. Go on an adventure to Tierra del Fuego and discover the best kept secrets of the Kawesqar and Selk’nam people, indigenous to the southernmost part of Chile.', '/images/MG_9778.jpg'),
+(3, 'Punta Arenas', '2021-01-01', '2040-12-31', '5.00', '`With port and sheep-breeding history, the gateway into Antartica will captivate you with its clean air and sophisticated European architecture. Take a boat or a kayak along the Magellan Straits and you’ll be surprised by the penguins as they welcome you to Magdalena Island. Go on an adventure to Tierra del Fuego and discover the best kept secrets of the Kawesqar and Selk’nam people, indigenous to the southernmost part of Chile.', '/images/MG_9778.jpg'),
 (17, 'Vancouver', '2021-01-01', '2021-01-07', '1000.00', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore consequuntur assumenda aspernatur dolore voluptatum nesciunt doloremque velit officia eius quia! Laudantium facere similique fugiat pariatur est. Quod repudiandae eveniet nihil.', 'https://res.cloudinary.com/simpleview/image/upload/v1486505969/clients/vancouverbc/Aerial_Sunset_Vancouver_d3_copy_1bb86ed0-1edc-4cda-841d-0b033ca0bb72.jpg'),
 (21, 'Nepal', '2021-01-01', '2021-01-07', '2000.00', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore consequuntur assumenda aspernatur dolore voluptatum nesciunt doloremque velit officia eius quia! Laudantium facere similique fugiat pariatur est. Quod repudiandae eveniet nihil.', 'https://mk0cs00242yfx7ww7i54.kinstacdn.com/wp-content/uploads/sites/5/nepal-village.jpg');
+(23, 'Haifa', '2021-01-01', '2021-01-07', '1.00', 'The Bahá’í Gardens (bahai gardens) are almost as famous as they are beautiful. This world heritage site is carved into the side of Mount Carmel, and is one of the most magnificent works of art you’ll ever lay eyes on. ', 'https://laurenonlocation.com/wp-content/uploads/2020/03/image10.png'),
+(27, 'The Moon', '2969-07-16', '2969-07-24', '999999.99', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore consequuntur assumenda aspernatur dolore voluptatum nesciunt doloremque velit officia eius quia! Laudantium facere similique fugiat pariatur est. Quod repudiandae eveniet nihil.', 'https://i.insider.com/5d2faa4d7e76cc3f20437ff6?width=1000&format=jpeg&auto=webp');
 
 --
 -- Indexes for dumped tables
@@ -217,19 +199,19 @@ ALTER TABLE `role`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `user_vacation`
 --
 ALTER TABLE `user_vacation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=422;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=458;
 
 --
 -- AUTO_INCREMENT for table `vacation`
 --
 ALTER TABLE `vacation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- Constraints for dumped tables
