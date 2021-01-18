d3e2e296df1fd07b9357c04c8c92c7f1df2f8ef6
ron <rinbar70@gmail.com>
2021-01-18 09:21:28 +0200

    Simplify getSqlConnection().

--- server/entities/connect.js
+++ server/entities/connect.js
@@ -2,26 +2,25 @@
 const mysql = require('mysql2/promise');
 const debug = require('debug')('server:mysql');
 
-const getSqlConnection = (function () {
-    let pool = null;
-    return async function () {
-        if (!pool) {
-            debug(`Connecting to \`${process.env['DBNAME']}\` at ${process.env['DBHOST'] || '(localhost)'}:${process.env['DBPORT'] || '(3306)'}...`);
-            pool = mysql.createPool({
-                host: process.env['DBHOST'],
-                port: process.env['DBPORT'],
-                user: process.env['DBUSER'],
-                password: process.env['DBPASS'],
-                database: process.env['DBNAME'],
-                dateStrings: [
-                    'DATE',
-                    'DATETIME',
-                ],
-            });
-        }
-        return await pool.getConnection();
-    };
-}());
+let pool = null;
+
+async function getSqlConnection() {
+    if (!pool) {
+        debug(`Connecting to \`${process.env['DBNAME']}\` at ${process.env['DBHOST'] || '(localhost)'}:${process.env['DBPORT'] || '(3306)'}...`);
+        pool = mysql.createPool({
+            host: process.env['DBHOST'],
+            port: process.env['DBPORT'],
+            user: process.env['DBUSER'],
+            password: process.env['DBPASS'],
+            database: process.env['DBNAME'],
+            dateStrings: [
+                'DATE',
+                'DATETIME',
+            ],
+        });
+    }
+    return await pool.getConnection();
+}
 
 exports.getSqlConnection = getSqlConnection;
 
