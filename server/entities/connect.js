022fb195fdf6fa1c0fc99d81f414135c4d410efc
ron <rinbar70@gmail.com>
2021-07-04 10:43:31 +0300

    Move DB URL parsing code into getSqlConnectionPool.

--- server/entities/connect.js
+++ server/entities/connect.js
@@ -4,24 +4,21 @@ const debug = require('debug');
 
 const log = debug('server:mysql');
 
-const DBURL_REGEX = /^mysql:\/\/(?:(?<user>\w+)(?::(?<password>\w+))?@)?(?<host>[\w.-]+)(?::(?<port>\d{4,5}))?(?:\/(?<database>\w+))?$/;
-
-const dburl = process.env['JAWSDB_MARIA_URL'] || 'mysql://localhost/vacations';
-
-const {
-    groups: {
-        host,
-        port,
-        user,
-        password,
-        database,
-    },
-} = dburl.match(DBURL_REGEX) || { groups: {} };
-
 let pool = null;
 
 function getSqlConnectionPool() {
     if (!pool) {
+        const DBURL_REGEX = /^mysql:\/\/(?:(?<user>\w+)(?::(?<password>\w+))?@)?(?<host>[\w.-]+)(?::(?<port>\d{4,5}))?(?:\/(?<database>\w+))?$/;
+        const dburl = process.env['JAWSDB_MARIA_URL'] || 'mysql://localhost/vacations';
+        const {
+            groups: {
+                host,
+                port,
+                user,
+                password,
+                database,
+            },
+        } = dburl.match(DBURL_REGEX) || { groups: {} };
         log(`Connecting to \`${database}\` at ${host || '(localhost)'}${port ? `:${port}` : '(:3306)'}...`);
         pool = mysql.createPool({
             host: host || 'localhost',
