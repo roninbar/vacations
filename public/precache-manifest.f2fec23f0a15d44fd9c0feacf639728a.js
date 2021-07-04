245988524938f202a9cd7a4777f59c96db75a4f8
ron <rinbar70@gmail.com>
2021-07-04 14:51:12 +0300

    Build the client and update /server/public.

--- public/precache-manifest.f2fec23f0a15d44fd9c0feacf639728a.js
+++ public/precache-manifest.f2fec23f0a15d44fd9c0feacf639728a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6b0bec8894838a5ad66241ffcbae26c6",
+    "revision": "e610ef344916135868642c3369819fdd",
     "url": "/index.html"
   },
   {
-    "revision": "23b2145c104e37481dc6",
+    "revision": "780d1bc05b615567e798",
     "url": "/static/css/main.0e96c88f.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.5048947e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "23b2145c104e37481dc6",
-    "url": "/static/js/main.5d1d97e7.chunk.js"
+    "revision": "780d1bc05b615567e798",
+    "url": "/static/js/main.68d9dd96.chunk.js"
   },
   {
     "revision": "9b4e396c183e42c1fa5c",
